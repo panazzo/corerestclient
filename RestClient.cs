c7c768145d5129fafff531c421a3717ae91df81d
Широбоков Андрей (Shirobokov_AV) <Shirobokov_AV@directum.ru>
2016-10-05 15:41:30 +0400

    Revert "Add Client property for reusing HttpClient"
    This reverts commit 4ab86e732c856d8f21e27325fcb00fabaec795e3.

--- RestClient.cs
+++ RestClient.cs
@@ -8,23 +8,9 @@ namespace corerestclient
     public class RestClient
     {
         private string authToken;
+
         private string contentType;
         private bool hasAuth;
-        private HttpClient client = new HttpClient();
-        
-        private HttpClient Client
-        {
-            get 
-            {
-                if(this.hasAuth)
-                {
-                    this.client.DefaultRequestHeaders.Add("Authorization", this.authToken);
-                }
-                this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(this.contentType));
-
-                return this.client;
-            }
-        }
 
         public RestClient()
         {
@@ -49,35 +35,63 @@ namespace corerestclient
         public string Post(string uri, string body)
         {
             byte[] byteData = Encoding.UTF8.GetBytes(body);
-            using (var content = new ByteArrayContent(byteData))
-            {
-                content.Headers.ContentType = new MediaTypeHeaderValue(this.contentType);
 
-                var response = Client.PostAsync(uri, content).Result;
-                return response.Content.ReadAsStringAsync().Result;
+            using (var client = GetHttpClient())
+            {
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(this.contentType);
+
+                    var response = client.PostAsync(uri, content).Result;
+                    return response.Content.ReadAsStringAsync().Result;
+                }
             }
         }
 
         public string Patch(string uri, string body)
         {
-            var method = new HttpMethod("PATCH");
-            var request = new HttpRequestMessage(method, uri) { Content = new StringContent(body, Encoding.UTF8, this.contentType) };
+            using (var client = GetHttpClient())
+            {
                 
-            var response = Client.SendAsync(request).Result;
-            return response.Content.ReadAsStringAsync().Result;
+                var method = new HttpMethod("PATCH");
+                var request = new HttpRequestMessage(method, uri) { Content = new StringContent(body, Encoding.UTF8, this.contentType) };
+                
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+
         }
 
         public string Get(string uri)
         {
-            var response = Client.GetAsync(uri).Result;
-            return response.Content.ReadAsStringAsync().Result;
+            using (var client = GetHttpClient())
+            {
+                var response = client.GetAsync(uri).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
         }
 
         public string Put(string uri, string resource, string content)
         {
-            Client.BaseAddress = new Uri(uri);
-            var requestContent = new StringContent(content, Encoding.UTF8, this.contentType);
-            return Client.PutAsync(resource, requestContent).Result.Content.ReadAsStringAsync().Result;
+            using (var client = GetHttpClient())
+            {
+                client.BaseAddress = new Uri(uri);
+                var requestContent = new StringContent(content, Encoding.UTF8, this.contentType);
+                return client.PutAsync(resource, requestContent).Result.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            if(this.hasAuth)
+            {
+                client.DefaultRequestHeaders.Add("Authorization", this.authToken);
+            }
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(this.contentType));
+
+            return client;
         }
     }
 }
