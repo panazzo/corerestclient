b36d9e1184dcf1d77f9fd4766f1507995ef19bed
Широбоков Андрей (Shirobokov_AV) <Shirobokov_AV@directum.ru>
2016-10-06 10:08:57 +0400

    add simpliest test for Get method without auth

---
+++ RestClient.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace corerestclient
+{
+    public class RestClient
+    {
+        private string authToken;
+        private string contentType;
+        private bool hasAuth;
+        private HttpClient client = new HttpClient();
+        
+        private HttpClient Client
+        {
+            get 
+            {
+                if(this.hasAuth)
+                {
+                    this.client.DefaultRequestHeaders.Add("Authorization", this.authToken);
+                }
+                this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(this.contentType));
+
+                return this.client;
+            }
+        }
+
+        public RestClient()
+        {
+            this.hasAuth = false;
+            this.contentType = "application/json";
+        }
+
+        public RestClient(string basicAuthToken)
+        {
+            this.hasAuth = true;
+            this.authToken = basicAuthToken;
+            this.contentType = "application/json";
+        }
+
+        public RestClient(string basicAuthToken, string contentType)
+        {
+            this.hasAuth = true;
+            this.authToken = basicAuthToken;
+            this.contentType = contentType;
+        }
+
+        public string Post(string uri, string body)
+        {
+            byte[] byteData = Encoding.UTF8.GetBytes(body);
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue(this.contentType);
+
+                var response = Client.PostAsync(uri, content).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        public string Patch(string uri, string body)
+        {
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, uri) { Content = new StringContent(body, Encoding.UTF8, this.contentType) };
+                
+            var response = Client.SendAsync(request).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public string Get(string uri)
+        {
+            var response = Client.GetAsync(uri).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public string Put(string uri, string resource, string content)
+        {
+            Client.BaseAddress = new Uri(uri);
+            var requestContent = new StringContent(content, Encoding.UTF8, this.contentType);
+            return Client.PutAsync(resource, requestContent).Result.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
--- test/corerestclient.tests/Tests.cs
+++ test/corerestclient.tests/Tests.cs
@@ -2,16 +2,16 @@
 using Xunit;
 using corerestclient;
 
-namespace Tests
+namespace corerestclient.Tests
 {
     public class Tests
     {
         [Fact]
-        public void Test1() 
+        public void GetWithoutAuth()
         {
             var client = new corerestclient.RestClient();
-            Console.WriteLine("Hello from test");
-            Assert.NotNull(client);
+            var result = client.Get("https://api.stackexchange.com/2.2/questions?page=1&pagesize=1&order=desc&sort=activity&site=stackoverflow");
+            Assert.False(string.IsNullOrEmpty(result));
         }
     }
 }
